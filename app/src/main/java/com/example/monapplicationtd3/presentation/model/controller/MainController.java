074bef168a6e36791de69c208db274db7bc5f4da
Joéla <novel-catin@et.esiea.fr>
2020-05-22 19:23:38 +0200

    Design List without PokeRepository

--- app/src/main/java/com/example/monapplicationtd3/presentation/model/controller/MainController.java
+++ app/src/main/java/com/example/monapplicationtd3/presentation/model/controller/MainController.java
@@ -1,19 +1,13 @@
 package com.example.monapplicationtd3.presentation.model.controller;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.widget.Toast;
 
 import com.example.monapplicationtd3.Constants;
 import com.example.monapplicationtd3.Singletons;
-import com.example.monapplicationtd3.data.PokeApi;
-import com.example.monapplicationtd3.data.PokeCallback;
-import com.example.monapplicationtd3.data.PokeRepository;
 import com.example.monapplicationtd3.presentation.model.Natures;
 import com.example.monapplicationtd3.presentation.model.RestPokemonResponse;
 import com.example.monapplicationtd3.presentation.model.view.MainActivity;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
@@ -22,21 +16,23 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainController {
 
-    private final PokeRepository pokeRepository;
+    //private final PokeRepository pokeRepository;
     private MainActivity view;
+    private Gson gson;
+    private SharedPreferences sharedPreferences;
 
-    public MainController(MainActivity mainActivity,PokeRepository pokeRepository){
+    public MainController(MainActivity mainActivity, Gson gson, SharedPreferences sharedPreferences){
         this.view = mainActivity;
-        this.pokeRepository = pokeRepository;
+        this.gson = gson;
+        this.sharedPreferences = sharedPreferences;
+        //this.pokeRepository = pokeRepository;
     }
 
-    public void onStart(){
-        pokeRepository.getPokemonResponse(new PokeCallback() {
+    /*public void onStart(){
+        //pokeRepository.getPokemonResponse(new PokeCallback() {
             @Override
             public void onSuccess(List<Natures> response) {
                 view.showList(response);
@@ -51,8 +47,51 @@ public class MainController {
 
     }
 
+*/
 
+    public void onStart(){
+        List<Natures> naturesList = getDataFromCache();
+        if(naturesList != null) {
+            view.showList(naturesList);
+        }else{
+            makeApiCall();
+        }
+    }
+    private void makeApiCall(){
+        Call<RestPokemonResponse> call = Singletons.getPokeApi().getPokemonResponse();
+        call.enqueue(new Callback<RestPokemonResponse>(){
+            public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response){
+                if(response.isSuccessful() && response.body() != null) {
+                    List<Natures> naturesList = response.body().getResults();
+                    saveList(naturesList);
+                    view.showList(naturesList);
+                }else{
+                    view.showError();
+                }
+            }
+            public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                view.showError();
+            }
+        });
+    }
 
+    private void saveList(List <Natures> naturesList) {
+        String jsonString = gson.toJson(naturesList);
+        sharedPreferences
+                .edit()
+                .putString(Constants.KEY_POKEMON_LIST, jsonString)
+                .apply();
+    }
+
+    private List<Natures> getDataFromCache(){
+        String jsonNatures = sharedPreferences.getString(Constants.KEY_POKEMON_LIST, null);
+        if(jsonNatures == null){
+            return null;
+        } else{
+            Type listType = new TypeToken<List<Natures>>(){}.getType();
+            return gson.fromJson(jsonNatures, listType);
+        }
+    }
 
     public void onItemClick(Natures natures){
         view.navigateToDetails(natures);
--- app/src/main/java/com/example/monapplicationtd3/presentation/model/view/MainActivity.java
+++ app/src/main/java/com/example/monapplicationtd3/presentation/model/view/MainActivity.java
@@ -34,9 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         controller = new MainController(
                 this,
-        this,
-
-
+        Singletons.getGson(),
+        Singletons.getSharedPreferences(getApplicationContext())
+        );
         controller.onStart();
         }
 
